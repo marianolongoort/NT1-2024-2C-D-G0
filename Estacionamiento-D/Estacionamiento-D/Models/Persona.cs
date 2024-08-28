10b3f29755aa1015fc1e5a93e9996188d6593f57
marianolongoort <mariano.longo@ort.edu.ar>
2024-08-27 22:56:13 -0300

    Popiedades de mis clases.

--- Estacionamiento-D/Estacionamiento-D/Models/Persona.cs
+++ Estacionamiento-D/Estacionamiento-D/Models/Persona.cs
@@ -1,6 +1,15 @@
-﻿namespace Estacionamiento_D.Models
+﻿using System.Collections.Generic;
+
+namespace Estacionamiento_D.Models
 {
     public class Persona
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        public Direccion Direccion { get; set; }
+
+        public List<Telefono> Telefonos { get; set; }
     }
 }
